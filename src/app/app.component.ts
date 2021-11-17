a6208506e16d3887d9ab2c3724bc6bb5af67b63d
Kat <moimakatlego2@gmail.com>
2021-11-17 20:56:03 +0200

    dist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router'; 
+import { filter } from 'rxjs/operators';
 
 declare let ga: Function;
 
@@ -12,13 +13,29 @@ export class AppComponent {
   title = 'OnlineStore';
 
 
-  constructor(public router: Router) {
+  // constructor(public router: Router) {
 
+  //   this.router.events.subscribe(event => {
+
+  //     if (event instanceof NavigationEnd) {
+  //       ga('set', 'page', event.urlAfterRedirects);
+  //       ga('send', 'pageview');
+  //     }
+  //   });
+  // }
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+      this.setUpAnalytics();
+  }
+
+  setUpAnalytics() {
     this.router.events.subscribe(event => {
-
       if (event instanceof NavigationEnd) {
         ga('set', 'page', event.urlAfterRedirects);
         ga('send', 'pageview');
+        
       }
     });
   }
